96e8362418f689e8c4d1ce828fc946a146396d2b
Kaito-Fujihira <k.f.alamoana@gmail.com>
2020-11-25 05:54:44 +0000

    Update categorys/question

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -69,11 +69,11 @@ $(function(){
     if($answer.hasClass('open')) {
       $answer.removeClass('open');
       $answer.slideUp();
-      $(this).find("span").text("+");
+      $(this).find("span").text("＋");
     } else {
       $answer.addClass('open');
       $answer.slideDown();
-      $(this).find("span").text("-");
+      $(this).find("span").text("ー");
     };
   });
 })
