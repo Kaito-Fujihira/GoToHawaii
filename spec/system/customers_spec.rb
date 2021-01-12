ff8332b41be6552f08948bae0483ae4c1ec4313d
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-01-12 05:25:33 +0000

    Fix インデント

--- spec/system/customers_spec.rb
+++ spec/system/customers_spec.rb
@@ -1,55 +1,55 @@
-require 'rails_helper'
+require "rails_helper"
 
-describe 'カスタマー認証のテスト' do
-  describe 'カスタマー新規登録' do
+describe "カスタマー認証のテスト" do
+  describe "カスタマー新規登録" do
     before do
       visit new_customer_registration_path
     end
 
-    context '新規登録画面に遷移' do
-      it '新規登録に成功する' do
-        fill_in 'customer[name]', with: Faker::Internet.customername(specifier: 6)
-        fill_in 'customer[email]', with: Faker::Internet.email
-        fill_in 'customer[password]', with: 'password'
-        fill_in 'customer[password_confirmation]', with: 'password'
+    context "新規登録画面に遷移" do
+      it "新規登録に成功する" do
+        fill_in "customer[name]", with: Faker::Internet.customername(specifier: 6)
+        fill_in "customer[email]", with: Faker::Internet.email
+        fill_in "customer[password]", with: "password"
+        fill_in "customer[password_confirmation]", with: "password"
         click_button '新規登録する'
 
-        expect(page).to have_content 'ようこそ！ アカウントが登録されました'
+        expect(page).to have_content "ようこそ！ アカウントが登録されました"
       end
-      it '新規登録に失敗する' do
-        fill_in 'user[name]', with: ''
-        fill_in 'user[email]', with: ''
-        fill_in 'user[password]', with: ''
-        fill_in 'user[password_confirmation]', with: ''
-        click_button '新規登録する'
+      it "新規登録に失敗する" do
+        fill_in "user[name]", with: ""
+        fill_in "user[email]", with: ""
+        fill_in "user[password]", with: ""
+        fill_in "user[password_confirmation]", with: ""
+        click_button "新規登録する"
 
-        expect(page).to have_content 'error'
+        expect(page).to have_content "error"
       end
     end
   end
 
-  describe 'カスタマーログイン' do
+  describe "カスタマーログイン" do
     let(:customer) { create(:customer) }
 
     before do
       visit new_customer_session_path
     end
 
-    context 'ログイン画面に遷移' do
+    context "ログイン画面に遷移" do
       let(:test_customer) { customer }
 
-      it 'ログインに成功する' do
-        fill_in 'customer[mail]', with: test_customer.name
-        fill_in 'customer[password]', with: test_customer.password
-        click_button 'ログイン'
+      it "ログインに成功する" do
+        fill_in "customer[mail]", with: test_customer.name
+        fill_in "customer[password]", with: test_customer.password
+        click_button "ログイン"
 
-        expect(page).to have_content 'ログインしました。'
+        expect(page).to have_content "ログインしました。"
       end
 
-      it 'ログインに失敗する' do
-        fill_in 'customer[mail]', with: ''
-        fill_in 'customer[password]', with: ''
-        click_button 'ログイン'
+      it "ログインに失敗する" do
+        fill_in "customer[mail]", with: ""
+        fill_in "customer[password]", with: ""
+        click_button "ログイン"
 
         expect(current_path).to eq(new_customer_session_path)
       end
