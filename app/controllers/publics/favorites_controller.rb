20760fb3b7bf5831e419d331d803f03cd33bb23d
Kaito-Fujihira <k.f.alamoana@gmail.com>
2021-01-12 04:58:17 +0000

    update いいね機能

--- app/controllers/publics/favorites_controller.rb
+++ app/controllers/publics/favorites_controller.rb
@@ -3,13 +3,17 @@ class Publics::FavoritesController < ApplicationController # いいね機能
 
   def create
     @post = Post.find(params[:post_id])
-    favorite = @post.favorites.new(customer_id: current_customer.id)
-    favorite.save
+    unless @post.favorited_by?(current_customer)
+      favorite = @post.favorites.new(customer_id: current_customer.id)
+      favorite.save
+    end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.find_by(post_id: @post.id)
-    favorite.destroy
+    if @post.favorited_by?(current_customer)
+      favorite = current_customer.favorites.find_by(post_id: @post.id)
+      favorite.destroy
+    end
   end
 end
